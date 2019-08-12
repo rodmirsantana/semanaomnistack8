e7bb4861aaf16768c0faf524c847e360505d3d46
rodmirsantana <rodmirandasantana@gmail.com>
2019-08-12 10:55:32 -0300

    Frontend concluído

---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; //Lib utilizada para aplicar o React ao ambiente do browser, que lida com a DOM
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
